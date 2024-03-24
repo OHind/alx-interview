c343cb58e24e1718a42f041534d9086684b6a32c
OHind <ouchani.hind@gmail.com>
2024-03-24 12:28:25 +0000

    Some pycode styling

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,7 +6,7 @@ def pascal_triangle(n):
     """Triangle"""
     if n <= 0:
         return []
-    my_list =[[1]]
+    my_list = [[1]]
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
